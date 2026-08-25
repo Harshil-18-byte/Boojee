78665070fcb8806b5175165e83c02d63a3beedee
Harshil-18-Byte <harshilwaghela189@gmail.com>
2026-08-25 12:08:55 +0530

    Fix Android hardware back button navigation for multi-page app

--- android/app/src/main/java/com/boojee/app/MainActivity.java
+++ android/app/src/main/java/com/boojee/app/MainActivity.java
@@ -1,5 +1,18 @@
 package com.boojee.app;
 
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        if (this.bridge != null) {
+            WebView webView = this.bridge.getWebView();
+            if (webView != null && webView.canGoBack()) {
+                webView.goBack();
+                return;
+            }
+        }
+        super.onBackPressed();
+    }
+}
